16e6d349afeddda9a1244ad6375343f6e4a24db1
Tianyun Xu <mxu37@smith.edu>
2016-08-17 02:21:45 +0900

    personal page set up

--- cs171_planet_earth/layout/assets/polar_tsunami/js/main.js
+++
@@ -1,7 +0,0 @@
-main();
-
-var tsunamiRunup;
-
-function main() {
-    tsunamiRunup = new TsunamiRunup("#tsunami-runup", "assets/polar_tsunami/data/tsunami_runup.tsv");
-}
---
+++ planet_earth/layout/assets/polar_tsunami/js/main.js
@@ -0,0 +1,7 @@
+main();
+
+var tsunamiRunup;
+
+function main() {
+    tsunamiRunup = new TsunamiRunup("#tsunami-runup", "assets/polar_tsunami/data/tsunami_runup.tsv");
+}
